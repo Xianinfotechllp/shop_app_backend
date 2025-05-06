8e17312b66f1dc97bf0dd1c2fbde816b1478d5b6
shanky-bhilwar <shankybhilwar24@gmail.com>
2025-05-06 13:50:50 +0530

    made an get api to fetch fixed categories from the api

--- src/controller/category.Controller.js
+++ src/controller/category.Controller.js
@@ -40,7 +40,54 @@ const getCategories = async (req, res) => {
   }
 };
 
+
+// shanky | making get api to fetch fixed category that we will provide in api only 
+
+const getFixedCategoryController = async (req,res) =>{
+
+  try{
+    //fixed category that we want to send from this get api
+    const categories = ["Grocery", "Bakery", "Pharmacy", "Electronics", "Mobile & Accessories", 
+                        "Clothing", "Footwear", "Home Appliances", "Furniture", "Stationery",
+                      "Toys & Games", "Books", "Beauty & Cosmetics", "Jewelry", 
+                      "Sports & Fitness", "Pet Supplies", "Automobile Accessories",
+                       "Hardware & Tools", "Florist", "Gift Shop", "Restaurant", "Cafe",
+                        "Fast Food", "Street Food", "Sweet Shop", "Tailor", "Salon/Barber",
+                         "Dry Cleaner", "Computer Repair", "Mobile Repair", "Fruits",
+                          "Vegetables", "Leafy Greens", "Herbs", "Bread", "Buns", "Cakes",
+                           "Cookies", "Pastries", "Chicken", "Mutton", "Fish", "Eggs",
+                            "Prawns", "Milk", "Curd", "Butter", "Cheese", "Ghee", 
+                            "Cooking Oil (Sunflower, Mustard, Coconut, etc.)", 
+                            "Rice (Basmati, Sona Masoori, etc.)", "Wheat Flour (Atta)",
+                             "Pulses (Toor Dal, Moong Dal, Chana Dal, etc.)", "Lentils", 
+                             "Biscuits", "Noodles", "Pasta", "Snacks", "Ready-to-eat meals", 
+                             "Tea", "Coffee", "Soft Drinks", "Juices", "Energy Drinks", 
+                             "Salt", "Sugar", "Turmeric", "Chilli Powder", "Garam Masala",
+                              "Pickles", "Soap", "Shampoo", "Toothpaste", "Sanitary Pads",
+                               "Shaving Cream", "Detergents", "Dishwash Liquid", 
+                               "Toilet Cleaner", "Air Fresheners", "Tissues"];
+
+            res.status(200).json({categories});
+
+  }catch(error){
+
+    res.status(500).json({message :"something went wrong while fetching the categories.."})
+  }
+}
+
+
+
+
 module.exports = {
   createCategory,
   getCategories,
+  getFixedCategoryController
 };
+
+
+
+
+
+
+
+
