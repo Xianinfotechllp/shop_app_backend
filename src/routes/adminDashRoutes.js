cbc7a970a9748b172203781824412c764f54ac06
shanky-bhilwar <shankybhilwar24@gmail.com>
2025-06-05 13:09:33 +0530

    created react app for shop app admin pannel
    created backend things of shop app admin pannel

---
+++ src/routes/adminDashRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const {handleGetAllUsers,handleGetUserById,handleUpdateUser,getUserLocation,updateUserLocation,getUserDetailsController} = require("../controller/user.controller");
+const shopController = require("../controller/shop.Controller");
+const {handleStartSubscription,handleCheckSubscriptionStatus,handleGetAllSubscriptions,} = require("../controller/subscription.controller");
+
+router.get("/getalluser",handleGetAllUsers);
+router.get("/getallshops",shopController.getShops);
+router.get("/get-all-subscription",  handleGetAllSubscriptions);
+//verifyToken, --> get all subscription
