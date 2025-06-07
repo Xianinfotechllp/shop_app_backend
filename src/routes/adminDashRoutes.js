df3879895c64234083157b2365c5fd7a900aa8c5
shanky-bhilwar <shankybhilwar24@gmail.com>
2025-06-07 10:50:18 +0530

    made new controller for fetching susbcription by user id
    and added middlewares in the getallsubscription api

--- src/routes/adminDashRoutes.js
+++ src/routes/adminDashRoutes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const {handleGetAllUsers,handleGetUserById,handleUpdateUser,deleteUserController,getUserLocation,updateUserLocation,getUserDetailsController} = require("../controller/user.controller");
 const shopController = require("../controller/shop.Controller");
-const {handleStartSubscription,handleCheckSubscriptionStatus,handleGetAllSubscriptions,} = require("../controller/subscription.controller");
+const {handleStartSubscription,handleCheckSubscriptionStatus,handleGetAllSubscriptions,handleSubscriptionByUser} = require("../controller/subscription.controller");
+const { verifyToken,verifyAdmin } = require("../middleware/verifyToken");
 
 router.get("/getalluser",handleGetAllUsers);
 router.get("/getallshops",shopController.getShops);
 router.delete("/deleteuser/:id",deleteUserController);
-router.get("/getallsubscription",  handleGetAllSubscriptions);
+router.get("/getallsubscription",verifyToken,verifyAdmin , handleGetAllSubscriptions);
+// only need admin token to access this route
+// need both token middleware in this to make this route work
+router.get("/subscription/byuserid/:userId",handleSubscriptionByUser);
 
 
 module.exports = router;
