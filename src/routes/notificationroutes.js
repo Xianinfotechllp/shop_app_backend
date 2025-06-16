53e0a117e30cb246eafc775887ec079661fa09bd
shanky-bhilwar <shankybhilwar24@gmail.com>
2025-06-16 15:57:37 +0530

    route addess fix of notification

--- src/routes/notificationroutes.js
+++ src/routes/notificationroutes.js
@@ -7,8 +7,8 @@ const {
 } = require("../controller/notification.controller");
 
 //index.js - route - /api/notification
-router.get("getnotificaitons/:userId", getUserNotifications); // GET unread notifications
+router.get("/getnotificaitons/:userId", getUserNotifications); // GET unread notifications
 router.put("/:notificationId/read", markNotificationRead); // PUT mark read/unread
-router.delete("deletenotificaiton/:notificationId", deleteNotificationForUser); // DELETE notification for user
+router.delete("/deletenotificaiton/:notificationId", deleteNotificationForUser); // DELETE notification for user
 
 module.exports = router;
